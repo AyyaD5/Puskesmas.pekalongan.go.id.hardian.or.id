554d974df83277cbcd2eb380aa72f5ebf86964b2
PekalonganCode <100528268+AyyaD5@users.noreply.github.com>
2022-07-18 09:17:05 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,43 @@
+<html xml:lang="en" lang="en" xmlns="http://www.w3.org/1999/xhtml"><head> 
+
+  <title>H4CKED BY G4P1T </title> 
+
+  <link rel="icon" href="https://c.top4top.io/p_2389t0uzz0.jpg" type="image/x-icon"> 
+
+  <meta property="og:image" content="https://l.top4top.io/p_2389w7cer0.jpeg"> 
+
+  <meta property="og:title" content="H4CK3D by MR•D0N4T"> 
+
+  <meta property="og:description" content="[  Pekalongan Code Team FT Bojonegoro Blackhat}"> 
+
+  <link href="https://fonts.googleapis.com/css?family=Inconsolata&amp;display=swap" rel="stylesheet"> 
+
+  <meta http-equiv="cache-control" content="index,cache"> 
+
+  <meta http-equiv="pragma" content="index,cache"> 
+
+  <meta name="theme-color" content="black"> 
+
+ </head> 
+
+ <body bgcolor="yellow"> 
+
+  <div style="height: auto; min-height: 50%;"> 
+
+   <div style="text-align: center; width:800px; margin-left: -400px; position: absolute; top: 30%; left: 50%;"> 
+
+    <img width="600" src="https://c.top4top.io/p_2389t0uzz0.jpg"> 
+
+    <br> 
+
+ <i><font size="8" face="Inconsolata" color="black">H4CK3D BY MR•G4P1T<br> <br>Toleransi Iku udu mong berdampingan tok nanging ugo soal sejajaran")<br> <br>xXx Pekalongan Code Team FT Bojonegoro Blackhat xXx<br><br> <p> <div class="mek"> <font color="green" class="text-center text-white mb-3" size="6">Greetz<center>-=|<marquee width="60%"></font><font class="text-center text-white mb-3" color="black" size="9">-./TeposNglarani7-Mr.PolEnk-icha 404-ToukaBagusDeweJare-AlipHakorAnakMamah-AwakuSengNulisDewe-insecX7-Dan All Team</font></marquee>|=-</center>
+
+      <audio src="https://g.top4top.io/m_2389hqmy50.mp3" autoplay loop="" controls=""></audio> </font></i> 
+
+   </div> 
+
+  </div> 
+
+ 
+
+</body></html>
